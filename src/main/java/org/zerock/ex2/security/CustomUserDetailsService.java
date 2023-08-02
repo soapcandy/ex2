f879b7aa9631b6cadde596b92f7d4bc2fdedcd96
soapcandy <octi97@naver.com>
2023-08-02 10:56:17 +0900

    회원정보를 가지고 오는 코드
    회원정보를 memberDTO로 전송

--- src/main/java/org/zerock/ex2/security/CustomUserDetailsService.java
+++ src/main/java/org/zerock/ex2/security/CustomUserDetailsService.java
@@ -6,7 +6,11 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.zerock.ex2.domain.Member;
+import org.zerock.ex2.dto.MemberDTO;
 import org.zerock.ex2.repository.MemberRepository;
+
+import java.util.stream.Collectors;
 
 @Service
 @Log4j2
@@ -14,6 +18,7 @@ import org.zerock.ex2.repository.MemberRepository;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
@@ -23,6 +28,24 @@ public class CustomUserDetailsService implements UserDetailsService {
         log.info("-----------------------loadUserByUserName-----------------------");
         log.info("-----------------------loadUserByUserName-----------------------");
 
-        return null;
+        Member member = memberRepository.getWithRoles(username);
+
+        if (member == null) {
+            throw new UsernameNotFoundException("Not Found");
+        }
+
+        MemberDTO memberDTO = new MemberDTO(
+                member.getEmail(),
+                member.getPw(),
+                member.getNickname(),
+                member.isSocial(),
+                member.getMemberRoleList()
+                        .stream()
+                        .map(memberRole -> memberRole.name()).collect(Collectors.toList()));
+
+        log.info(memberDTO);
+
+        return memberDTO;
+
     }
 }
